aad18a3ebe6b8c387276cae1f9064c141961a2c9
Hannah Elliott <hgelliott90@gmail.com>
2021-01-19 19:58:36 -0500

    Starting server.js.

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const db = require('./models');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
