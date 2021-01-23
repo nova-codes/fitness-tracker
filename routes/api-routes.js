5fe8573cae5ebb6859fa684536d13b5046ebc5f3
Hannah Elliott <hgelliott90@gmail.com>
2021-01-23 11:54:03 -0500

    Updating api routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -19,3 +19,42 @@ router.get('/api/workouts', (req, res) => {
         res.json(err);
     });
 });
+
+// updates one workout by adding an exercise
+router.put('/api/workouts/:id', (req,res) => {
+    db.Fitness.findByIdAndUpdate(req.params.id, {
+        $push: {
+            exercises: req.body
+        }
+    })
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+// create a new workout
+router.post('/api/workouts/range', (req, res) => {
+    db.Workout.aggregate([
+        {
+            $sort: { day: -1 } 
+        },
+        {
+            $limit: 7
+        },
+        { $addFields: {
+            totalDuration: {$sum: '$exercises.duration'}
+        }
+    }
+    ])
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+module.exports = router;
